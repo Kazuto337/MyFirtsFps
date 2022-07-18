74eed9c3343bb2cff75630b9d1892aabe454343f
Santiago Lara <60271850+Kazuto337@users.noreply.github.com>
2022-07-18 14:39:42 -0500

    Camera Movement pt2
    New player rotation
    Player Movement Fixed

--- My Firts FPS/Assets/Scripts/PlayerController.cs
+++ My Firts FPS/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
     InputManager inputManager;
     [SerializeField] CharacterController characterController;
-    [SerializeField] float speed;
-    Transform cameraTransform;
+    [SerializeField] float speed, mouseSens;
+    [SerializeField] Transform cameraTransform , playerBody;
+    [SerializeField] CinemachineVirtualCamera vCamera;
 
     private void Start()
     {
         inputManager = InputManager.Instance;
-        cameraTransform = Camera.main.transform;
+        vCamera.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = mouseSens;
+        vCamera.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = mouseSens;
     }
 
     private void Update()
     {
+        float mouseHorizontal = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+
         Vector2 inputMovement = inputManager.GetPlayerMovement();
         Vector3 move = transform.right * inputMovement.x + transform.forward * inputMovement.y;
         move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
         move.y = 0f;
+
+        playerBody.rotation = Quaternion.Euler(0f, vCamera.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.Value, 0f); 
+
         characterController.Move(move * speed * Time.deltaTime);
     }
 }
