dd8755aa5a39b0b4ced356148352408bd509f0a6
Santiago Lara <60271850+Kazuto337@users.noreply.github.com>
2022-07-18 22:46:06 -0500

    Aplying Gravity
    Fixed Collisions Bug

--- My Firts FPS/Assets/Scripts/PlayerController.cs
+++ My Firts FPS/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,24 @@ using Cinemachine;
 public class PlayerController : MonoBehaviour
 {
     InputManager inputManager;
+    [Header("Movement")]
     [SerializeField] CharacterController characterController;
     [SerializeField] float speed, mouseSens;
-    [SerializeField] Transform cameraTransform , playerBody;
+
+    [Header("Camera Movement")]
+    [SerializeField] Transform cameraTransform, playerBody;
     [SerializeField] CinemachineVirtualCamera vCamera;
+
+    [SerializeField] float gravityValue, jumpSpeed , groundDistance = 0.4f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+    bool isGrounded;
+    Vector3 velocity;
 
     private void Start()
     {
+        velocity.y += gravityValue * Time.deltaTime;
+
         inputManager = InputManager.Instance;
         vCamera.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.m_MaxSpeed = mouseSens;
         vCamera.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis.m_MaxSpeed = mouseSens;
@@ -20,6 +31,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position , groundDistance , groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+        
+        velocity.y += gravityValue * Time.deltaTime;
+
         float mouseHorizontal = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
 
         Vector2 inputMovement = inputManager.GetPlayerMovement();
@@ -27,8 +47,9 @@ public class PlayerController : MonoBehaviour
         move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
         move.y = 0f;
 
-        playerBody.rotation = Quaternion.Euler(0f, vCamera.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.Value, 0f); 
+        playerBody.rotation = Quaternion.Euler(0f, vCamera.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis.Value, 0f);
 
         characterController.Move(move * speed * Time.deltaTime);
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
